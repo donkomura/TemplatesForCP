c3e7aed9e61a320be28ab2032c429ff448fafd8a
kage// <koiru3822fs@gmail.com>
2018-12-09 18:22:39 +0900

    テンプレートの出入力を追加

--- template.cpp
+++ template.cpp
@@ -3,14 +3,16 @@
 #define erep(i,a,b) for(int i=a;i<=(int)(b);++i)
 #define per(i,a,b) for(int i=(a);i>(b);--i)
 #define eper(i,a,b) for(int i=(a);i>=b;--i)
+#define fore(i, x, a) for(auto &&x:a) 
+#define ITR(i,b,e) for(auto i=(b);i!=(e);++i)
 #define pb push_back
 #define mp make_pair
-#define INF 100100100100
-#define inf 2001001001
 #define MOD 1000000007
 #define ALL(x) begin(x),end(x)
 #define F first
 #define S second
+const int inf=1001001001;
+const long long INF=1001001001001001001;
 using namespace std;
 using ll = long long;
 using pii = pair<int,int>;
@@ -20,15 +22,20 @@ using priority_queue_small = priority_queue<int, vector<int>, greater<int> >;
 template<class T>using vv = vector<T>;
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
-template<class T>void show(const vector<T> &a) { for (int i = 0; i < (int)a.size(); ++i) cout << a[i] << (i != (int)a.size()-1 ? " " : "\n"); }
+template<class T> ostream& operator<<(ostream &os,const vector<T> &v) {
+    ITR(i,begin(v),end(v))os<<*i<<(i==end(v)-1?"":" ");return os;}
+template<class T> istream& operator>>(istream &is,vector<T> &v) {
+    ITR(i,begin(v),end(v)) is>>*i;return is;}
+template<class T,class U> istream& operator>>(istream &is, pair<T,U> &p) {
+    is>>p.first>>p.second;return is;}
 template<class T>T gcd(T a, T b){ return b ? gcd(b, a % b) : a; }
 template<class T>T lcm(T a, T b){ return a / gcd(a, b) * b; }
 int dy[]={0, 1, -1, 0};
 int dx[]={1, 0, 0, -1};
 
 int main() {
-  cin.tie(0);
-  ios::sync_with_stdio(false);
+ cin.tie(0);
+ ios::sync_with_stdio(false);
 
-   return 0;
+  return 0;
 }
